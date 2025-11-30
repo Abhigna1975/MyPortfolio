6e2d2ba6a58494695b2e722d9601cee50d304f76
Abhigna1975 <margamabhigna@gmail.com>
2025-11-29 20:32:57 -0600

    removing header.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import '../styles/Hero.css';
 import ProfileImage from '../assets/Profile.jpg';
 
-const ResumePDF = '/Abhigna Resume.pdf'; // Update with your exact filename
+const ResumePDF = '../assets/Abhigna Resume.pdf'; // Update with your exact filename
 
 const Hero = ({ activeTab, setActiveTab }) => {
   const [isMobile, setIsMobile] = useState(false);
