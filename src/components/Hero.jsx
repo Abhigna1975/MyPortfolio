b244d03afe9ad51115dd5dd05f580beeaefe6549
Abhigna1975 <margamabhigna@gmail.com>
2025-12-14 16:22:27 -0600

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import '../styles/Hero.css';
 import ProfileImage from '../assets/Profile.jpg';
 
-const ResumePDF = '/Abhigna_Resume.pdf';
+const ResumePDF = '/Abhigna Resume.pdf';
 
 const Hero = ({ activeTab, setActiveTab }) => {
   const [isMobile, setIsMobile] = useState(false);
